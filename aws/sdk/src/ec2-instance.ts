881229ebb8d80eb65f190fac2b5e4dad4eef9d81
Oleksii Gontar <f.gontar5@gmail.com>
2024-08-30 10:37:23 +0300

    sync command

--- aws/sdk/src/ec2-instance.ts
+++ aws/sdk/src/ec2-instance.ts
@@ -5,6 +5,7 @@ import { flatten } from 'lodash';
 import { join } from 'path';
 import { STATE_DIR } from './constants';
 import { readFile, writeFile } from 'fs/promises';
+import { homedir } from 'os';
 
 const program = new Command();
 
@@ -78,6 +79,50 @@ program.command('stop')
     await client.send(new StopInstancesCommand({
       InstanceIds: [instanceId],
     }));
+  });
+
+const SERVICE_PREFIX = '# AWS-Server:';
+
+program.command('sync')
+  .action(async () => {
+    const configRaw = await readFile(CONFIG_PATH);
+    const config = JSON.parse(configRaw.toString());
+    const { Reservations } = await client.send(new DescribeInstancesCommand());
+    if (!Reservations) {
+      console.log('No reservations');
+      return;
+    }
+    const dnsNames = flatten(Reservations.map((it) => it.Instances!)).reduce((acc, { InstanceId, PublicDnsName }) => {
+      if (!InstanceId || !PublicDnsName) {
+        return acc;
+      }
+      acc[InstanceId] = PublicDnsName;
+      return acc;
+    }, {} as Record<string, string>);
+    const sshConfigPath = join(homedir(), '.ssh', 'config');
+    const sshConfigRaw = await readFile(sshConfigPath);
+    const sshConfig = sshConfigRaw.toString();
+    const newSshConfig = sshConfig.split('\n').map((line) => {
+      if (line.includes(SERVICE_PREFIX)) {
+        const hostStart = line.indexOf('ec2');
+        const hostEnd = line.indexOf(SERVICE_PREFIX) - 1;
+        const serviceStart = hostEnd + SERVICE_PREFIX.length + 2;
+        const serviceName = line.substring(serviceStart).trim();
+        const instanceId = config[serviceName];
+        if (!instanceId) {
+          console.log('No instance found');
+          return line;
+        }
+        const dnsName = dnsNames[instanceId];
+        if (!dnsName) {
+          console.log('Dns name not found');
+          return line;
+        }
+        return line.substring(0, hostStart) + dnsName + line.substring(hostEnd);
+      }
+      return line;
+    }).join('\n');
+    await writeFile(sshConfigPath, newSshConfig);
   });
 
 program.parseAsync().catch((e) => console.error('Error during parsing', e));
